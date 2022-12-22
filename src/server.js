8a5bc9fbd50dcb2ddc3230478972ac250fbd277f
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-22 13:07:29 +0200

    Creating a listings endpoint

--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
 import Hapi from "@hapi/hapi"
-
+import routes from "./routes";
 const start = async () =>{
     const server = Hapi.server({
        port: 8000,
        host:'localhost',
     });
 
-    server.route({
-        metthod:'GET',
-        path:'/hello',
-        handler: (req,h) =>{
-            return 'hello world'
-        }
-    })
+    routes.forEach(route=> server.route(route))
     await server.start();
     console.log(`Server is listening on ${server.info.uri}`);
 }
