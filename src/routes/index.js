71d2acf22e6f4e378a77a206e171cf1c793275b0
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-22 17:38:29 +0000

    'Creating an endpoint to get a user listing'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { addViewToListingRoute } from "./addViewToListing";
+import { getUserListingsRoute } from "./getUserListings";
+
 export default [
     getAllListingsRoute,
     getListingRoute,
-    addViewToListingRoute
+    addViewToListingRoute,
+    getUserListingsRoute
+
 ]
