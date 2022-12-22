4352136ce11dfc6b85a9473bf224bbc1f4689877
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-22 13:19:11 +0200

    Error handling with boom

--- src/routes/getListing.js
+++ src/routes/getListing.js
@@ -1,3 +1,4 @@
+import  Boom  from '@hapi/boom'
 import {fakeListings} from './fake-data'
 
 export const getListingRoute={
@@ -5,6 +6,9 @@ export const getListingRoute={
     path:'/api/listings/{id}',
     handler:(req,h)=>{
         const id = req.params.id
-        return fakeListings.find(listing => listing.id === id)
+        const listing = fakeListings.find(listing => listing.id === id)
+        if(!listing) throw Boom.notFound(`Listing not found with id: ${id}`)
+        return listing;
     }
+
 }
