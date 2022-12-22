b256256dc5949233a4272f976e3ff1525f24928e
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-22 17:34:21 +0000

    'Endpoint for adding views'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
-
+import { addViewToListingRoute } from "./addViewToListing";
 export default [
     getAllListingsRoute,
-    getListingRoute
+    getListingRoute,
+    addViewToListingRoute
 ]
