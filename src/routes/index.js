af1350f8a4aeb6e19d740f8e5d720e11ce920aa0
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-22 18:26:53 +0000

    'Edit listings endpoint'

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import { getListingRoute } from "./getListing";
 import { addViewToListingRoute } from "./addViewToListing";
 import { getUserListingsRoute } from "./getUserListings";
 import { createNewListingRoute } from "./createNewListing";
+import { updateListingRoute } from "./updateListing";
 
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
     getUserListingsRoute,
-    createNewListingRoute
+    createNewListingRoute,
+    updateListingRoute
 
 ]
