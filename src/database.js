5da2bae855edb5a127bd919043f7f21f0bc5eee4
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-22 17:24:54 +0000

    'Connect mysql database and rewrite listings endpoints'

---
+++ src/database.js
@@ -0,0 +1,17 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user:'theplague',
+    password:'',
+    database:'buy_and_sell'
+})
+
+export const db = {
+    connect:()=> connection.connect(),
+    query:(queryString,escapedValues)=> new Promise((resolve,reject)=> connection.query(queryString,escapedValues,(error,results,fields)=>{
+        if(error) reject(error)
+        resolve({results,fields});
+    })),
+    end:()=> connection.end()
+}
