121763710b7badeb52c6dca63ea407f744774e91
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-22 12:49:37 +0200

    Setting up hapi server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import Hapi from "@hapi/hapi"
+
+const start = async () =>{
+    const server = Hapi.server({
+       port: 8000,
+       host:'localhost',
+    });
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
+}
+
+process.on('uhandledRejection',(err)=>{
+    console.log(err);
+    process.exit(1);
+})
+
+start();
